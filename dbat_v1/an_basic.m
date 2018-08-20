9f9a05f2f8ba805649b959b553ccc87e0e29b347
jrmxn <j.mcintoshr@gmail.com>
2018-08-20 12:57:25 -0400

    plot

--- dbat_v1/an_basic.m
+++ dbat_v1/an_basic.m
@@ -1,7 +1,7 @@
-d = load('C:\Users\jmcin\Local\gitprojects\ptb_experiments\dbat_v1\data\temp_data.mat');
+d = load('C:\Users\jmcin\Local\gitprojects\ptb_experiments\dbat_v1\data\fres_180820125323_SUB00_00.mat');
 d = d.data;
 clf;hold on;
 plot([d.result.upper_bound_inst]);
-plot([d.result.x]);
-plot([d.result.x_obs]);
+plot([d.result.x],'g');
+plot([d.result.x_obs], 'g--');
 plot([d.result.lower_bound_inst]);
