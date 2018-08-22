a13d80090fa1ff9e892330e6b2bfbb3229a67ef3
jrmxn <j.mcintoshr@gmail.com>
2018-08-22 12:12:22 -0400

    gen

---
+++ dbat_v2/an_basic.m
@@ -0,0 +1,11 @@
+d_loc = dir('data');
+d_loc = d_loc(contains({d_loc.name},'fres'));
+[~,ix] = max([d_loc.datenum]);
+d_loc = d_loc(ix);
+d = load(fullfile(d_loc.folder, d_loc.name));
+d = d.data;
+clf;hold on;
+plot([d.result.upper_bound_inst]);
+plot([d.result.x],'g');
+plot([d.result.x_obs], 'g--');
+plot([d.result.lower_bound_inst]);
